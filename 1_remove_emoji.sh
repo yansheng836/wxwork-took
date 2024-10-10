92ce3cbde995e819b4697edc0f1cc79551c5b0e3
yansheng836 <sheng.yan836@gmail.com>
2024-10-10 13:53:50 +0800

    feat: update 1_remove_emoji.sh

--- 1_remove_emoji.sh
+++ 1_remove_emoji.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # 把小于10kb的照片，当做是表情包（当然实际上不止这些，因为在企微表情包就是图片来的，好像没有什么规律，暂时只是按照大小来计算）
-find ./2023-11 -size -10K
-find ./2023-11 -size -10K rm -rf 
+find ./ -type f -size -15k -exec rm -f {} \;
+find ./ -type f -name "*charset*" -exec rm -f {} \;
+find ./ -type f -name ".WeDrive" -exec rm -f {} \;
+find ./ -type d -empty -exec rm -rf {} \;
