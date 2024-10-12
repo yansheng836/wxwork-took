c78144a89e938c4189c64308497a3745059e0e31
yansheng836 <sheng.yan836@gmail.com>
2024-10-12 22:19:47 +0800

    chore: update install config

--- 运行脚本-自动打包.bat
+++ 运行脚本-自动打包.bat
@@ -9,17 +9,17 @@ echo 正在打包……
 ::-i                指定Windows icon图标
 ::--version-file    指定软件版权信息
 ::-n                指定文件名
-pyinstaller.exe --clean -y -F -i favicon.ico --version-file file_version_info.txt -n xuanwu-oa-xml2json zhiyuan_oa_xml_for.py
+pyinstaller.exe --clean -y -F -i favicon.ico --version-file file_version_info.txt -n 企微的工具包 main.py
 
 ::运行方式2：用spec生成
 ::pyinstaller.exe xuanwu-excel2json.spec
 
-pandoc README.md  -o .\dist\xuanwu-oa-xml2json使用说明.docx
+pandoc README.md  -o .\dist\企微的工具包-使用说明.docx
 
-mkdir .\release\latest 
-del /q/f .\release\latest\*
-copy .\dist  .\release\latest /Y /D
-copy .\*.xml .\release\latest /Y /D
+mkdir        .\release\latest
+del   /q/f   .\release\latest\*
+copy .\dist  .\release\latest  /Y /D
+copy .\*.xml .\release\latest  /Y /D
 
 echo 打包结束，详见 ./release/latest 目录。
 pause
--- 运行脚本.bat
+++ 运行脚本.bat
@@ -4,6 +4,6 @@ echo 正在安装必要工具包……
 pip install -r requirements.txt
 
 echo 正在安装运行脚本……
-py zhiyuan_oa_xml_for.py
+py main.py
 pause
 exit
