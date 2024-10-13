c2290603933fa7149401ab4900d15e5466c882dc
yansheng836 <sheng.yan836@gmail.com>
2024-10-13 14:33:15 +0800

    feat: 使用普通for循环

--- 4_remove_duplicate_content_pic.py
+++ 4_remove_duplicate_content_pic.py
@@ -7,17 +7,21 @@ directory = r'D:\WXWork\1688850205828112\Cache\Image\2023-07'
 # 遍历目录下的所有文件
 file_list = os.listdir(directory)
 dup_list = []
-for filename in file_list:
+
+# 用简单的for循环，方便控制
+for i in range(0, len(file_list)):
+    # print(i)
+    filename = file_list[i]
+    print(filename)
     # 获取文件的完整路径
     full_path = os.path.join(directory, filename)
     # print(f"外层循环的文件:{filename}")
     with open(full_path, 'rb') as f:
         file_hash = hashlib.md5(f.read()).hexdigest()
 
-    for filename2 in file_list:
-        if filename == filename2:
-            continue
+    for j in range(i+1, len(file_list)):
         # 获取文件的完整路径
+        filename2 = file_list[j]
         full_path2 = os.path.join(directory, filename2)
         # print(f" 内层循环的文件:{filename2}")
         with open(full_path2, 'rb') as f2:
